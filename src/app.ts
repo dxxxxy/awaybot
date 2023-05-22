24d81c3199a46d99631e46b9a3df99d61c16ca21
dxxxxy <introsdream@gmail.com>
2023-05-22 17:47:21 -0400

    feat: better join flow

--- src/app.ts
+++ src/app.ts
@@ -20,23 +20,47 @@ const interest = /You have just received (.*) coins as interest in your personal
 bot.addChatPattern("allowance", allowance)
 bot.addChatPattern("interest", interest)
 
-bot.on("login", () => {
-    console.log(`awaybot[${bot.username}] - Logged in.`)
+bot.once("spawn", () => {
+    console.log(`awaybot[${bot.username}] - Spawned.`)
+    warpToSkyblock()
+    warpToIsland()
 })
 
-bot.on("spawn", () => {
+const warpToSkyblock = () => {
     if (!inSkyblock()) {
         //the command is inconsistent, this ensures it works
         const retryTimer = setInterval(() => {
-            if (inSkyblock()) clearInterval(retryTimer)
+            if (inSkyblock()) {
+                console.log("Joined skyblock.")
+                clearInterval(retryTimer)
+            }
             else bot.chat("/play sb")
         }, 5000)
-    } else console.log("Already in Skyblock")
-})
+    }
+}
+
+const warpToIsland = () => {
+    if (!inPrivateIsland()) {
+        //the command is inconsistent, this ensures it works
+        const retryTimer = setInterval(() => {
+            if (!inSkyblock()) return
+
+            if (inPrivateIsland()) {
+                console.log("Teleported to private island.")
+                clearInterval(retryTimer)
+            }
+            else bot.chat("/warp home")
+        }, 5000)
+    }
+}
 
 //logging
 bot.on("error", err => {
     appendFileSync("error.log", `${new Date(Date.now()).toLocaleTimeString()} - ${err}`)
+})
+
+bot.on("actionBar", message => {
+    console.log(message)
 })
 
 const inSkyblock = () => {
@@ -44,18 +68,19 @@ const inSkyblock = () => {
 }
 
 const inPrivateIsland = () => {
+    //weird emojis for some reason, possibly a bug with mineflayer
     return bot.scoreboard.sidebar.items.map(item => item.displayName).join("\n").replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, "").includes("Your Island")
 }
 
 //chat patterns
-// @ts-ignore
+//@ts-ignore
 bot.on("chat:allowance", (_, message: String) => {
     const coins = message.match(allowance)[1]
     stats.allowance += Number.parseInt(coins)
     console.log(`Got ${coins} coins from allowance.`)
 })
 
-// @ts-ignore
+//@ts-ignore
 bot.on("chat:interest", (_, message: String) => {
     const coins = message.match(interest)[1]
     stats.interest += Number.parseInt(coins)
