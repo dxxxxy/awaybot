71e5c3425a0b30a4b0680d9e74a2cee8cebdf8df
dxxxxy <introsdream@gmail.com>
2023-05-27 14:14:27 -0400

    fix: mineflayer event matching + wrong bi pattern

--- src/event/value/allowance.ts
+++ src/event/value/allowance.ts
@@ -1,11 +1,10 @@
 //@ts-ignore
 import { bot, stats } from "../../app.js"
-import { allowance } from "../../util/patterns.js"
 import { log } from "../../util/utils.js"
 
 //@ts-ignore
-bot.on("chat:allowance", (_, message: string) => {
-    const amount = allowance.exec(message)[1]
+bot.on("chat:allowance", (matches: String[]) => {
+    const amount = matches[0]
     stats.allowance += parseInt(amount.replace(/,/g, ""))
     log(`+${amount} coins from allowance`, "yellow")
 })
--- src/util/patterns.ts
+++ src/util/patterns.ts
@@ -1,3 +1,3 @@
 export const allowance = /ALLOWANCE! You earned (.*) coins!/
-export const interest = /You have just received (.*) coins as interest in your personal bank account!/
+export const interest = /You have just received (.*) coins as bank interest!/
 export const bits = /\+(.*) Bits from Cookie Buff!/
