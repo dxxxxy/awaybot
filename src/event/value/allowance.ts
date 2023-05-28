e9c3f2643c999a637e6f3ef5bc5d886bfc0a5f5f
dxxxxy <introsdream@gmail.com>
2023-05-28 01:50:09 -0400

    fix: pattern group

--- src/event/value/allowance.ts
+++ src/event/value/allowance.ts
@@ -1,10 +1,11 @@
 //@ts-ignore
 import { bot, stats } from "../../app.js"
 import { log } from "../../util/utils.js"
+import { allowance } from "../../util/patterns.js"
 
 //@ts-ignore
-bot.on("chat:allowance", (matches: String[]) => {
-    const amount = matches[0]
+bot.on("chat:allowance", (matches: string[]) => {
+    const amount = allowance.exec(matches[0])[1]
     stats.allowance += parseInt(amount.replace(/,/g, ""))
     log(`+${amount} coins from allowance`, "yellow")
 })
