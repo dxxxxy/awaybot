88feded9f835aadaac930092f6102b3a69d8efd5
Serhiy <owner@dreamys.studio>
2025-12-22 22:53:48 -0500

    feat: change variables from static to runtime & move json out of dist

--- src/util/statManager.ts
+++ src/util/statManager.ts
@@ -1,43 +1,34 @@
 import { existsSync, readFileSync, writeFileSync } from "fs"
 
+const FILE_NAME = "../stats.json"
+
 export default class StatManager {
-    static allowance: number = 0
-    static interest: number = 0
-    static bits: number = 0
-
     static init() {
-        //prefill ghost values
-        Object.keys(this).forEach(key => this["_" + key] = this[key])
-
-        //setup get/set hooks for auto save with ghost values
-        Object.keys(this).forEach(key => {
-            if (key.startsWith("_")) return
-            Object.defineProperty(this, key, {
-                set: (val) => {
-                    this["_" + key] = val
-
-                    //save
-                    this.save()
-                },
-                get: () => this["_" + key]
+        //if a previous stat file exists, register and load
+        if (existsSync(FILE_NAME)) {
+            Object.entries(JSON.parse(readFileSync(FILE_NAME, "utf8"))).forEach(([key, value]) => {
+                StatManager.register(key);
+                this[key] = value
             })
-        })
-
-        //load previous values if they exist
-        if (existsSync("stats.json")) Object.entries(this.read()).forEach(([key, value]) => this[key] = value)
+        }
 
         console.log("[StatManager] OK")
     }
 
-    private static read() {
-        return JSON.parse(readFileSync("stats.json", "utf8"))
-    }
+    static register(key: string) {
+        if (this[key]) return
+        this[`_${key}`] = this[key] = 0
+        Object.defineProperty(this, key, {
+            set: (val) => {
+                this["_" + key] = val
 
-    private static save() {
-        const object = Object.entries(this)
-            .filter(([key]) => key.startsWith("_"))
-            .map(([key, value]) => [key.replace("_", ""), value])
+                const object = Object.entries(this)
+                    .filter(([key]) => key.startsWith("_"))
+                    .map(([key, value]) => [key.replace("_", ""), value])
 
-        writeFileSync("stats.json", JSON.stringify(Object.fromEntries(object), null, 4))
+                writeFileSync(FILE_NAME, JSON.stringify(Object.fromEntries(object), null, 4))
+            },
+            get: () => this["_" + key]
+        })
     }
 }
