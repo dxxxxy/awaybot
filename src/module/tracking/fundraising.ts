cbc6fc6ea7b3f1f98d0b9fd14ed7df9e56b1073a
Serhiy <owner@dreamys.studio>
2025-12-24 00:46:33 -0500

    fix: correct fundraising pattern ending

--- src/module/tracking/fundraising.ts
+++ src/module/tracking/fundraising.ts
@@ -1,7 +1,7 @@
 import { Bot } from "mineflayer"
 import SimpleChatTracker from "../../util/simpleChatTracker.js"
 
-const fundraising = /UNIVERSAL INCOME: You gained (.*) Coins./
+const fundraising = /UNIVERSAL INCOME: You gained (.*) Coins\./
 
 // noinspection JSUnusedGlobalSymbols
 export default (bot: Bot) => new SimpleChatTracker("fundraising", fundraising, "coins").start(bot)
