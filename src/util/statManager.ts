87ecdee06f2f4c24e69c52e8ec81174ce3894d53
Serhiy <owner@dreamys.studio>
2026-01-01 15:59:41 -0500

    feat: add default value stat register + comment

--- src/util/statManager.ts
+++ src/util/statManager.ts
@@ -4,28 +4,31 @@ const FILE_NAME = "../stats.json"
 
 export default class StatManager {
     static init() {
-        //if a previous stat file exists, register and load
+        //if a previous stat file exists, register key value pairs
         if (existsSync(FILE_NAME)) {
-            Object.entries(JSON.parse(readFileSync(FILE_NAME, "utf8"))).forEach(([key, value]) => {
-                StatManager.register(key);
-                this[key] = value
-            })
+            Object.entries(JSON.parse(readFileSync(FILE_NAME, "utf8"))).forEach(([key, value]) => StatManager.register(key, value))
         }
-
-        console.log("[StatManager] OK")
     }
 
-    static register(key: string) {
-        if (this[key]) return
-        this[`_${key}`] = this[key] = 0
+    static register(key: string, value: any = 0) {
+        //avoid reregistering keys
+        if (this[key]) return console.warn(`[StatManager] Stat "${key}" is already registered! Skipping...`)
+
+        //setup variables (the ghost _variable stores the actual value, while the real variable is the custom getter/setter)
+        this[`_${key}`] = this[key] = value
+
+        //define getter and setter
         Object.defineProperty(this, key, {
             set: (val) => {
+                //update ghost value
                 this["_" + key] = val
 
+                //create object with ghost keys stripped of _ to reflect real stat names
                 const object = Object.entries(this)
                     .filter(([key]) => key.startsWith("_"))
                     .map(([key, value]) => [key.replace("_", ""), value])
 
+                //write all stats to file
                 writeFileSync(FILE_NAME, JSON.stringify(Object.fromEntries(object), null, 4))
             },
             get: () => this["_" + key]
